acdaac3c80e95d8e90cc585340bc11fbbd45ed16
Ray Chidaz <raychidaz@gmail.com>
2021-04-27 13:17:21 +0100

    creating JS utiltiy that modifies answers, load and use the utiltiy on client, updating the script to modify upvotes on  server as well

--- client/client.jsx
+++ client/client.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { handleModifyAnswerVotes } from '../shared/utility';
 
 let state = undefined;
 
@@ -12,23 +13,19 @@ fetch('http://localhost:7777/data')
     render();
   });
 
-// ReactDOM.render(<App />, document.querySelector('#Container'));
+function handleVote(answerId, increment) {
+  state.answers = handleModifyAnswerVotes(state.answers, answerId, increment);
 
-function handleModifyAnswerVotes(answerId, increment) {
-  state.answers = state.answers.map((answer) => {
-    if (answer.answerId !== answerId) {
-      return answer;
-    } else {
-      return { ...answer, upvotes: answer.upvotes + increment };
-    }
-  });
+  fetch(`vote/${answerId}?increment=${increment}`);
 
   render();
 }
 
+// ReactDOM.render(<App />, document.querySelector('#Container'));
+
 function render() {
   ReactDOM.hydrate(
-    <App {...state} handleModifyAnswerVotes={handleModifyAnswerVotes} />,
+    <App {...state} handleModifyAnswerVotes={handleVote} />,
     document.querySelector('#Container'),
   );
 }
